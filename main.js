090c61697ca1d7d73ed1958c457343284196236b
Eric Maranga <ekirira22@gmail.com>
2024-01-14 14:09:12 +0300

    completed assignment

--- main.js
+++ main.js
@@ -2,6 +2,41 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
+//Get all like buttons:
+const init = () =>{
+  document.querySelector('div#modal').className = 'hidden'
+  
+    const likeglyphs = document.querySelectorAll('.like-glyph')
+    likeglyphs.forEach(glyph => {
+    glyph.addEventListener('click', e => {
+      let glyphicon = e.target.textContent
+      if(glyphicon == EMPTY_HEART){
+        mimicServerCall(e)
+        try{
+          e.target.textContent = FULL_HEART
+          e.target.className = 'activated-heart'
+        }catch(e){
+          document.querySelector('div#modal').classList.remove('hidden')
+        }
+        
+      }else{
+        mimicServerCall(e)
+        try{
+          e.target.classList.remove('activated-heart')
+          e.target.textContent = EMPTY_HEART
+          
+        }catch(e){
+          
+        }
+    }
+
+      // console.log(glyphicon == '♡')
+    })
+  })
+}
+
+document.addEventListener('DOMContentLoaded', init)
 // Your JavaScript code goes here!
 
 
